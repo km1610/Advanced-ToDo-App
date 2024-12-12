e9d561c41e01fbebe6a5c93039450bbfa4a6d2f9
K. M. <90254516+km1610@users.noreply.github.com>
2024-12-12 14:49:44 +0530

    Update views.py

--- ToDo_App/App/views.py
+++ ToDo_App/App/views.py
@@ -41,8 +41,6 @@ def user_login(request):
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
